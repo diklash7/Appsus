b0d582898607e645691917eeb6573a2d3e93e0f2
dikla cohen <diklash7@gmail.com>
2022-03-02 17:23:19 +0200

    adding filter

--- js/apps/keep/cmps/note-list.cmp.js
+++ js/apps/keep/cmps/note-list.cmp.js
@@ -1,16 +1,21 @@
-
+import notePreview from './note-preview.cmp.js'
 export default {
-
+    props: ['notes'],
     template: `
-<section class="note-list">
-<ul>
-<li v-for> </li>
-</ul>
+        <section class="note-list">
+            <input type="text"  placeholder="What's on your mind...">
+            <ul>
+                <li v-for="note in notes" :key="note.id"> 
+                    <note-preview :note="note"/>
 
+                </li>
+            </ul>
+        </section>
 
-</section>
-
-`
+`,
+    components: {
+        notePreview
+    },
 
 
 
