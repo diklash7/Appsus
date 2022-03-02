72d34f451d2a81a27635c73b726247fb3901ab4d
dikla cohen <diklash7@gmail.com>
2022-03-02 15:34:37 +0200

    save to storage

--- js/apps/keep/pages/note-app.cmp.js
+++ js/apps/keep/pages/note-app.cmp.js
@@ -1,7 +1,22 @@
+import { noteService } from "../services/note.service.js";
+
 export default {
     template:`
         <section class="note-app app-main">
             <h3>noteapp</h3>
         </section>
     `,
+
+data() {
+    return {
+        notes: null,
+        // selectedBook: null,
+        // filterBy: null
+    };
+
+},
+created(){
+    noteService.query()
+    .then(notes=> this.notes=notes);
+}
 }
